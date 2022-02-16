43fd2ff9b82d62be85f989f66da4c428f141541c
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2022-02-16 14:13:44 -0700

    initial templating structure

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': "Bryan Beck",
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2022'
+    },
+    {
+        'author': "Jane Doe",
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'August 28, 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
